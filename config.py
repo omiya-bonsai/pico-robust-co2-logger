0c05137fc98f039de4c642691bc908253c1fae8d
omiya-bonsai <179561497+omiya-bonsai@users.noreply.github.com>
2025-07-31 08:22:09 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+WIFI_SSID = 'あなたのWIFI_SSID'
+WIFI_PASSWORD = 'あなたのWIFI_PASSWORD'
+
+MQTT_SERVER = '192.168.1.100' # あなたのMQTTブローカーのIPアドレス
+MQTT_PORT = 1883
+MQTT_CLIENT_ID = 'pico_w_co2_monitor_living_room' # このデバイスのユニークなID
